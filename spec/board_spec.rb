a24e7d8f0cd4c827fc63ee8f9080af49d33432ee
Gunnar <dunkcrunk.com>
2021-04-04 00:20:49 -0600

    refactored to look better

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -44,4 +44,13 @@ RSpec.describe Board do
     expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq(true)
     expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq(true)
   end
+
+  it 'can place ships' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    expect().to eq()
+  end
 end
